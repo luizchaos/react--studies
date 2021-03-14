0232571d9125836f222115a15d36f69ef9ae1500
Henrique Carvalho <henrique@MacBook-Pro.local>
2021-03-14 19:07:07 -0300

    added components

---
+++ src/ItemComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ItemComponent(props){
+    const status = props.status;
+    return(
+        <li>{props.name}
+            <div>Status: {status ? <div>Finalizado</div> : <div>Nao Finalizado</div>}</div>
+            
+        </li>
+    ) 
+}
---
+++ src/UserLists.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ListComponent from './ListComponent'
+
+export default class UserList extends React.Component
+{
+    state = {lists: [], loading: true}
+    async componentDidMount(){
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        
+        config.headers['Authorization'] = 'Token '+ localStorage.getItem('token')
+
+        var url = 'http://localhost:8000/list/';
+        const response = await fetch(url, config);
+        const data = await response.json();
+        console.log(data);
+        this.setState({lists: data, loading: false})
+    }
+
+    render(){
+        const listApi = this.state.lists;
+
+        return(
+            <div>
+                {listApi.map(list => <ListComponent key={list.id} listName={list.name} items={list.item_set}/>)}
+            </div>
+        )
+
+        
+    }   
+}
